54a5a1b7c19ed23eff78eb93ecadf327d756716a
Subhobrata Dey <sbcd90@gmail.com>
2022-06-08 01:53:36 +0000

    add skeleton for the security-analytics plugin
    Signed-off-by: Subhobrata Dey <sbcd90@gmail.com>

---
+++ src/test/java/org/opensearch/securityanalytics/SecurityAnalyticsPluginIT.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.opensearch.securityanalytics;
+
+import org.junit.Assert;
+import org.opensearch.test.OpenSearchIntegTestCase;
+
+public class SecurityAnalyticsPluginIT extends OpenSearchIntegTestCase {
+
+    public void testNothing() {
+        Assert.assertTrue(true);
+    }
+}
