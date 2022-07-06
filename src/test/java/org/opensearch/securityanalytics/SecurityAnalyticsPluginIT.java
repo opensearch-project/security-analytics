e6df155e78562d7a1ffbc28701545b13c1cb8aa6
marko <okrammarko@gmail.com>
2022-07-06 16:09:34 -0600

    added REST routes with stubs for creating and executing monitors.

--- src/test/java/org/opensearch/securityanalytics/SecurityAnalyticsPluginIT.java
+++ src/test/java/org/opensearch/securityanalytics/SecurityAnalyticsPluginIT.java
@@ -6,8 +6,9 @@ package org.opensearch.securityanalytics;
 
 import org.junit.Assert;
 import org.opensearch.test.OpenSearchIntegTestCase;
+import org.opensearch.test.OpenSearchTestCase;
 
-public class SecurityAnalyticsPluginIT extends OpenSearchIntegTestCase {
+public class SecurityAnalyticsPluginIT extends OpenSearchTestCase {
 
     public void testNothing() {
         Assert.assertTrue(true);
