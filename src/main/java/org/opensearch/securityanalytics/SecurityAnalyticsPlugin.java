45e772a62d2f9cd994f36c1e429d9e9455febd04
marko <okrammarko@gmail.com>
2022-07-06 16:09:34 -0600

    added REST routes with stubs for creating and executing monitors.
    Signed-off-by: marko <okrammarko@gmail.com>

--- src/main/java/org/opensearch/securityanalytics/SecurityAnalyticsPlugin.java
+++ src/main/java/org/opensearch/securityanalytics/SecurityAnalyticsPlugin.java
@@ -4,7 +4,48 @@
  */
 package org.opensearch.securityanalytics;
 
+import org.opensearch.cluster.metadata.IndexNameExpressionResolver;
+import org.opensearch.cluster.node.DiscoveryNodes;
+import org.opensearch.common.settings.ClusterSettings;
+import org.opensearch.common.settings.IndexScopedSettings;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.common.settings.SettingsFilter;
+import org.opensearch.monitor.MonitorService;
+import org.opensearch.plugins.ActionPlugin;
 import org.opensearch.plugins.Plugin;
+import org.opensearch.plugins.PluginsService;
+import org.opensearch.rest.RestController;
+import org.opensearch.rest.RestHandler;
 
-public class SecurityAnalyticsPlugin extends Plugin {
+import javax.management.monitor.Monitor;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class SecurityAnalyticsPlugin extends Plugin implements ActionPlugin {
+
+    private Plugin alerting;
+
+    @Override
+    public List<RestHandler> getRestHandlers(
+            final Settings settings,
+            final RestController restController,
+            final ClusterSettings clusterSettings,
+            final IndexScopedSettings indexScopedSettings,
+            final SettingsFilter settingsFilter,
+            final IndexNameExpressionResolver indexNameExpressionResolver,
+            final Supplier<DiscoveryNodes> nodesInCluster) {
+        return List.of(new SecurityAnalyticsCat(this));
+    }
+
+    public SecurityAnalyticsPlugin() {
+
+    }
+
+    public void createMonitor() {
+
+    }
+
+    public void executeMonitor() {
+
+    }
 }
