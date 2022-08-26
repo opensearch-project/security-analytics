0704f43f8efbc51cec19de098e0061c968a1c144
jiahe zhang <zhanncha@amazon.com>
2022-08-26 11:38:53 -0700

    adding licenseHeaders
    Signed-off-by: jiahe zhang <zhanncha@amazon.com>

--- src/main/java/org/opensearch/securityanalytics/mappings/MapperApplier.java
+++ src/main/java/org/opensearch/securityanalytics/mappings/MapperApplier.java
@@ -1,3 +1,8 @@
+/*
+Copyright OpenSearch Contributors
+SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.opensearch.securityanalytics.mappings;
 
 import org.opensearch.action.admin.indices.mapping.put.PutMappingRequest;
--- src/main/java/org/opensearch/securityanalytics/mappings/MapperFacade.java
+++ src/main/java/org/opensearch/securityanalytics/mappings/MapperFacade.java
@@ -1,3 +1,8 @@
+/*
+Copyright OpenSearch Contributors
+SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.opensearch.securityanalytics.mappings;
 import java.io.File;
 import java.io.FileInputStream;
