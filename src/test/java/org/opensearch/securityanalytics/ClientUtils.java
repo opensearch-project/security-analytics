979f74c19233ac07481e5d94c5181026a6c99c7e
Petar Dzepina <petar.dzepina@gmail.com>
2022-09-07 18:47:57 +0200

    added licence header
    Signed-off-by: Petar Dzepina <petar.dzepina@gmail.com>

--- src/test/java/org/opensearch/securityanalytics/ClientUtils.java
+++ src/test/java/org/opensearch/securityanalytics/ClientUtils.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.opensearch.securityanalytics;
 
 import org.opensearch.action.admin.indices.mapping.get.GetMappingsResponse;
