7dd0bd89547d9b1c39b3687a421736b3dbab81cb
Petar Dzepina <petar.dzepina@gmail.com>
2022-10-26 20:02:05 +0200

    reverted rule IT
    Signed-off-by: Petar Dzepina <petar.dzepina@gmail.com>

--- src/test/java/org/opensearch/securityanalytics/resthandler/RuleRestApiIT.java
+++ src/test/java/org/opensearch/securityanalytics/resthandler/RuleRestApiIT.java
@@ -33,33 +33,9 @@ import static org.opensearch.securityanalytics.TestHelpers.*;
 public class RuleRestApiIT extends SecurityAnalyticsRestTestCase {
 
     public void testCreatingARule() throws IOException {
-
-        // Create Detector
-        String index = createTestIndex(randomIndex(), windowsIndexMapping());
-
-        // Execute CreateMappingsAction to add alias mapping for index
-        Request createMappingRequest = new Request("POST", SecurityAnalyticsPlugin.MAPPER_BASE_URI);
-        // both req params and req body are supported
-        createMappingRequest.setJsonEntity(
-                "{ \"index_name\":\"" + index + "\"," +
-                        "  \"rule_topic\":\"windows\", " +
-                        "  \"partial\":true" +
-                        "}"
-        );
-
-        Response response = client().performRequest(createMappingRequest);
-        assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
-
-        Detector detector = randomDetector(getRandomPrePackagedRules());
-
-        Response createResponse = makeRequest(client(), "POST", SecurityAnalyticsPlugin.DETECTOR_BASE_URI, Collections.emptyMap(), toHttpEntity(detector));
-        Assert.assertEquals("Create detector failed", RestStatus.CREATED, restStatus(createResponse));
-
-        // Create rule
-
         String rule = randomRule();
 
-        createResponse = makeRequest(client(), "POST", SecurityAnalyticsPlugin.RULE_BASE_URI, Collections.singletonMap("category", "windows"),
+        Response createResponse = makeRequest(client(), "POST", SecurityAnalyticsPlugin.RULE_BASE_URI, Collections.singletonMap("category", "windows"),
                 new StringEntity(rule), new BasicHeader("Content-Type", "application/json"));
         Assert.assertEquals("Create rule failed", RestStatus.CREATED, restStatus(createResponse));
 
@@ -71,7 +47,7 @@ public class RuleRestApiIT extends SecurityAnalyticsRestTestCase {
         Assert.assertTrue("incorrect version", createdVersion > 0);
         Assert.assertEquals("Incorrect Location header", String.format(Locale.getDefault(), "%s/%s", SecurityAnalyticsPlugin.RULE_BASE_URI, createdId), createResponse.getHeader("Location"));
 
-        index = Rule.CUSTOM_RULES_INDEX;
+        String index = Rule.CUSTOM_RULES_INDEX;
         String request = "{\n" +
                 "  \"query\": {\n" +
                 "    \"nested\": {\n" +
