c1902313ddc039634abb5ff3e3a9074d4c895c57
Petar Dzepina <petar.dzepina@gmail.com>
2022-11-03 21:16:44 +0100

    Rollover for alerts/findings history indices (#82)
    Signed-off-by: Petar Dzepina <petar.dzepina@gmail.com>
    (cherry picked from commit 95f8d9a877b1a97f8b8f63d63669a5b0afb0e9be)

--- src/main/java/org/opensearch/securityanalytics/settings/SecurityAnalyticsSettings.java
+++ src/main/java/org/opensearch/securityanalytics/settings/SecurityAnalyticsSettings.java
@@ -4,6 +4,7 @@
  */
 package org.opensearch.securityanalytics.settings;
 
+import java.util.concurrent.TimeUnit;
 import org.opensearch.common.settings.Setting;
 import org.opensearch.common.unit.TimeValue;
 
@@ -12,4 +13,86 @@ public class SecurityAnalyticsSettings {
     public static Setting<TimeValue> INDEX_TIMEOUT = Setting.positiveTimeSetting("plugins.security_analytics.index_timeout",
             TimeValue.timeValueSeconds(60),
             Setting.Property.NodeScope, Setting.Property.Dynamic);
+
+    public static final Long DEFAULT_MAX_ACTIONABLE_ALERT_COUNT = 50L;
+
+    public static final Setting<Boolean> ALERT_HISTORY_ENABLED = Setting.boolSetting(
+            "plugins.security_analytics.alert_history_enabled",
+            true,
+            Setting.Property.NodeScope, Setting.Property.Dynamic
+    );
+
+    public static final Setting<Boolean> FINDING_HISTORY_ENABLED = Setting.boolSetting(
+            "plugins.security_analytics.alert_finding_enabled",
+            true,
+            Setting.Property.NodeScope, Setting.Property.Dynamic
+    );
+
+    public static final Setting<TimeValue> ALERT_HISTORY_ROLLOVER_PERIOD = Setting.positiveTimeSetting(
+            "plugins.security_analytics.alert_history_rollover_period",
+            TimeValue.timeValueHours(12),
+            Setting.Property.NodeScope, Setting.Property.Dynamic
+    );
+
+    public static final Setting<TimeValue> FINDING_HISTORY_ROLLOVER_PERIOD = Setting.positiveTimeSetting(
+            "plugins.security_analytics.alert_finding_rollover_period",
+            TimeValue.timeValueHours(12),
+            Setting.Property.NodeScope, Setting.Property.Dynamic
+    );
+
+    public static final Setting<TimeValue> ALERT_HISTORY_INDEX_MAX_AGE = Setting.positiveTimeSetting(
+            "plugins.security_analytics.alert_history_max_age",
+            new TimeValue(30, TimeUnit.DAYS),
+            Setting.Property.NodeScope, Setting.Property.Dynamic
+    );
+
+    public static final Setting<TimeValue> FINDING_HISTORY_INDEX_MAX_AGE = Setting.positiveTimeSetting(
+            "plugins.security_analytics.finding_history_max_age",
+            new TimeValue(30, TimeUnit.DAYS),
+            Setting.Property.NodeScope, Setting.Property.Dynamic
+    );
+
+    public static final Setting<Long> ALERT_HISTORY_MAX_DOCS = Setting.longSetting(
+            "plugins.security_analytics.alert_history_max_docs",
+            1000L,
+            0L,
+            Setting.Property.NodeScope, Setting.Property.Dynamic
+    );
+
+    public static final Setting<Long> FINDING_HISTORY_MAX_DOCS = Setting.longSetting(
+            "plugins.security_analytics.alert_finding_max_docs",
+            1000L,
+            0L,
+            Setting.Property.NodeScope, Setting.Property.Dynamic, Setting.Property.Deprecated
+    );
+
+    public static final Setting<TimeValue> ALERT_HISTORY_RETENTION_PERIOD = Setting.positiveTimeSetting(
+            "plugins.security_analytics.alert_history_retention_period",
+            new TimeValue(60, TimeUnit.DAYS),
+            Setting.Property.NodeScope, Setting.Property.Dynamic
+    );
+
+    public static final Setting<TimeValue> FINDING_HISTORY_RETENTION_PERIOD = Setting.positiveTimeSetting(
+            "plugins.security_analytics.finding_history_retention_period",
+            new TimeValue(60, TimeUnit.DAYS),
+            Setting.Property.NodeScope, Setting.Property.Dynamic
+    );
+
+    public static final Setting<TimeValue> REQUEST_TIMEOUT = Setting.positiveTimeSetting(
+            "plugins.security_analytics.request_timeout",
+            TimeValue.timeValueSeconds(10),
+            Setting.Property.NodeScope, Setting.Property.Dynamic
+    );
+
+    public static final Setting<TimeValue> MAX_ACTION_THROTTLE_VALUE = Setting.positiveTimeSetting(
+            "plugins.security_analytics.action_throttle_max_value",
+            TimeValue.timeValueHours(24),
+            Setting.Property.NodeScope, Setting.Property.Dynamic
+    );
+
+    public static final Setting FILTER_BY_BACKEND_ROLES = Setting.boolSetting(
+            "plugins.security_analytics.filter_by_backend_roles",
+            false,
+            Setting.Property.NodeScope, Setting.Property.Dynamic
+    );
 }
