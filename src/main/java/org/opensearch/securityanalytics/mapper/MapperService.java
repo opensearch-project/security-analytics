500265dca7b405aa2d4021c29e9b3231356ca0b2
Petar Dzepina <petar.dzepina@gmail.com>
2023-01-11 22:57:12 +0100

    bugfix
    Signed-off-by: Petar Dzepina <petar.dzepina@gmail.com>

--- src/main/java/org/opensearch/securityanalytics/mapper/MapperService.java
+++ src/main/java/org/opensearch/securityanalytics/mapper/MapperService.java
@@ -5,16 +5,9 @@
 
 package org.opensearch.securityanalytics.mapper;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
