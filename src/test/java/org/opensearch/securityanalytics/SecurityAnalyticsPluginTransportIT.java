f950cc077c83b26c9bbb636816e978ddb6dd6394
Subhobrata Dey <sbcd90@gmail.com>
2023-01-13 02:03:17 +0000

    add change
    Signed-off-by: Subhobrata Dey <sbcd90@gmail.com>

--- src/test/java/org/opensearch/securityanalytics/SecurityAnalyticsPluginTransportIT.java
+++ src/test/java/org/opensearch/securityanalytics/SecurityAnalyticsPluginTransportIT.java
@@ -2,7 +2,7 @@
  * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
  */
-package org.opensearch.securityanalytics;
+package org.opensearch.securityanalytics;    
 
 import org.junit.Assert;
 import org.opensearch.action.admin.cluster.node.info.NodeInfo;
