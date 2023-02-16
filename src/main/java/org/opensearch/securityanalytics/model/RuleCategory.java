640670a657486bc3c31b66ac7986003bf24a24d0
Petar Dzepina <petar.dzepina@gmail.com>
2023-02-16 20:52:19 +0100

    added loading categories from file
    Signed-off-by: Petar Dzepina <petar.dzepina@gmail.com>

--- src/main/java/org/opensearch/securityanalytics/model/RuleCategory.java
+++ src/main/java/org/opensearch/securityanalytics/model/RuleCategory.java
@@ -6,16 +6,32 @@
 package org.opensearch.securityanalytics.model;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.opensearch.OpenSearchParseException;
 import org.opensearch.common.io.stream.StreamInput;
 import org.opensearch.common.io.stream.StreamOutput;
 import org.opensearch.common.io.stream.Writeable;
+import org.opensearch.common.settings.SettingsException;
 import org.opensearch.common.xcontent.ToXContentObject;
 import org.opensearch.common.xcontent.XContentBuilder;
+import org.opensearch.common.xcontent.XContentHelper;
+import org.opensearch.common.xcontent.json.JsonXContent;
+import org.opensearch.securityanalytics.mapper.MapperTopicStore;
 
 public class RuleCategory implements Writeable, ToXContentObject {
+
+    private static final Logger log = LogManager.getLogger(RuleCategory.class);
 
     public static final String KEY = "key";
     public static final String DISPLAY_NAME = "display_name";
@@ -51,21 +67,61 @@ public class RuleCategory implements Writeable, ToXContentObject {
                 .endObject();
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    private void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    private static final String RULE_CATEGORIES_CONFIG_FILE = "rules/rule_categories.json";
+
+    // Rule category is the same as detector type
     public static final List<RuleCategory> ALL_RULE_CATEGORIES;
 
     static {
         List<RuleCategory> ruleCategories = new ArrayList<>();
-        ruleCategories.add(new RuleCategory(Detector.DetectorType.AD_LDAP.getDetectorType(), "AD/LDAP", true));
-        ruleCategories.add(new RuleCategory(Detector.DetectorType.DNS.getDetectorType(), "DNS logs", true));
-        ruleCategories.add(new RuleCategory(Detector.DetectorType.NETWORK.getDetectorType(), "Netflow", true));
-        ruleCategories.add(new RuleCategory(Detector.DetectorType.APACHE_ACCESS.getDetectorType(), "Apache access logs", true));
-        ruleCategories.add(new RuleCategory(Detector.DetectorType.CLOUDTRAIL.getDetectorType(), "Cloud Trail logs", true));
-        ruleCategories.add(new RuleCategory(Detector.DetectorType.S3.getDetectorType(), "S3 access logs", true));
-        ruleCategories.add(new RuleCategory(Detector.DetectorType.WINDOWS.getDetectorType(), "Windows logs", true));
-        ruleCategories.add(new RuleCategory(Detector.DetectorType.LINUX.getDetectorType(), "System logs", true));
+        String ruleCategoriesJson;
+        try (
+                InputStream is = RuleCategory.class.getClassLoader().getResourceAsStream(RULE_CATEGORIES_CONFIG_FILE)
+        ) {
+            ruleCategoriesJson = new String(Objects.requireNonNull(is).readAllBytes(), StandardCharsets.UTF_8);
 
+            if (ruleCategoriesJson != null) {
+                Map<String, Object> configMap =
+                        XContentHelper.convertToMap(JsonXContent.jsonXContent, ruleCategoriesJson, false);
+                List<Map<String, Object>> categories = (List<Map<String, Object>>) configMap.get("rule_categories");
+                for (Map<String, Object> c : categories) {
+                    ruleCategories.add(new RuleCategory(
+                            (String) c.get(KEY),
+                            (String) c.get(DISPLAY_NAME),
+                            (Boolean) c.get(ENABLED)
+                    ));
+                }
+            }
+        } catch (OpenSearchParseException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new SettingsException("Failed to load settings from [" + RULE_CATEGORIES_CONFIG_FILE + "]", e);
+        }
+        // Go through all categories from config and disable ones which are not found in DetectorType enum.
+        // Disabling rule category would prevent it from showing on UI on "Create Detector" page
+        List<String> detectorTypes = Arrays.stream(Detector.DetectorType.values())
+                .map(Detector.DetectorType::getDetectorType)
+                .collect(Collectors.toList());
+
+        for (RuleCategory c : ruleCategories) {
+            if (detectorTypes.contains(c.name) == false) {
+                c.setEnabled(false);
+                log.warn("Rule category: [" + c.getName() + "] from config is not present in DetectorType enum!");
+            }
+        }
         ALL_RULE_CATEGORIES = Collections.unmodifiableList(ruleCategories);
     }
-
 }
 
