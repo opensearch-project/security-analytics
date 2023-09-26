9a10743019f1908dae2392f5b74c7b7695e22d7c
Sandesh Kumar <sandeshkr419@gmail.com>
2023-09-26 12:49:45 -0700

    test fix
    Signed-off-by: Sandesh Kumar <sandeshkr419@gmail.com>

--- src/test/java/org/opensearch/securityanalytics/resthandler/RuleRestApiIT.java
+++ src/test/java/org/opensearch/securityanalytics/resthandler/RuleRestApiIT.java
@@ -770,7 +770,7 @@ public class RuleRestApiIT extends SecurityAnalyticsRestTestCase {
     public void testGetAllRuleCategories() throws IOException {
         Response response = makeRequest(client(), "GET", SecurityAnalyticsPlugin.RULE_BASE_URI + "/categories", Collections.emptyMap(), null);
         List<Object> categories = (List<Object>) asMap(response).get("rule_categories");
-        assertEquals(22, categories.size());
+        assertEquals(23, categories.size());
         assertTrue(categories.stream().anyMatch(e -> ((Map<String, Object>)e).get("key").equals("ad_ldap")));
         assertTrue(categories.stream().anyMatch(e -> ((Map<String, Object>)e).get("key").equals("dns")));
         assertTrue(categories.stream().anyMatch(e -> ((Map<String, Object>)e).get("key").equals("network")));
@@ -783,5 +783,6 @@ public class RuleRestApiIT extends SecurityAnalyticsRestTestCase {
         assertTrue(categories.stream().anyMatch(e -> ((Map<String, Object>)e).get("key").equals("okta")));
         assertTrue(categories.stream().anyMatch(e -> ((Map<String, Object>)e).get("key").equals("azure")));
         assertTrue(categories.stream().anyMatch(e -> ((Map<String, Object>)e).get("key").equals("linux")));
+        assertTrue(categories.stream().anyMatch(e -> ((Map<String, Object>)e).get("key").equals("waf")));
     }
 }
