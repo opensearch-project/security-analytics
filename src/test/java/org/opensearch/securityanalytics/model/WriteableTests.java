e850248cbfcd1cd5e48807248a25333b3b3decc8
Surya Sashank Nistala <snistala@amazon.com>
2023-10-24 22:08:08 -0700

    fix workflow failing test
    Signed-off-by: Surya Sashank Nistala <snistala@amazon.com>

--- src/test/java/org/opensearch/securityanalytics/model/WriteableTests.java
+++ src/test/java/org/opensearch/securityanalytics/model/WriteableTests.java
@@ -19,7 +19,7 @@ import static org.opensearch.securityanalytics.TestHelpers.randomUserEmpty;
 
 public class WriteableTests extends OpenSearchTestCase {
 
-    public void testDetectorAsStrea() throws IOException {
+    public void testDetectorAsAStream() throws IOException {
         Detector detector = randomDetector(List.of());
         detector.setInputs(List.of(new DetectorInput("", List.of(), List.of(), List.of())));
         BytesStreamOutput out = new BytesStreamOutput();
