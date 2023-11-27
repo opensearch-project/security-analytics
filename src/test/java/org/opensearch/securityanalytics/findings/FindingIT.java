4cca068a1ab5a859cc0b6d6fe477d002c7815e47
Joanne Wang <109310487+jowg-amazon@users.noreply.github.com>
2023-11-27 15:40:41 -0500

    Ignore flaky test (#730)
    * flaky test
    
    Signed-off-by: Joanne Wang <jowg@amazon.com>
    
    * ignore flaky test
    
    Signed-off-by: Joanne Wang <jowg@amazon.com>
    
    ---------
    
    Signed-off-by: Joanne Wang <jowg@amazon.com>

--- src/test/java/org/opensearch/securityanalytics/findings/FindingIT.java
+++ src/test/java/org/opensearch/securityanalytics/findings/FindingIT.java
@@ -264,7 +264,7 @@ public class FindingIT extends SecurityAnalyticsRestTestCase {
         getFindingsBody = entityAsMap(getFindingsResponse);
         Assert.assertEquals(1, getFindingsBody.get("total_findings"));
     }
-
+    @Ignore
     public void testGetFindings_rolloverByMaxAge_success() throws IOException, InterruptedException {
 
         updateClusterSetting(FINDING_HISTORY_ROLLOVER_PERIOD.getKey(), "1s");
