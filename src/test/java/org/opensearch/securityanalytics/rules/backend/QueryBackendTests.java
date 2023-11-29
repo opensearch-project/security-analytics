1e9d5d896a3a14d3192b7189c62e03e1c5d25979
Surya Sashank Nistala <snistala@amazon.com>
2023-11-29 15:59:47 -0800

    fix null query filter conversion from sigma to query string query (#722)
    * fix null query filter conversion from sigma to query string query
    
    Signed-off-by: Surya Sashank Nistala <snistala@amazon.com>
    
    * fix rule to query conversion tests for null filter
    
    Signed-off-by: Surya Sashank Nistala <snistala@amazon.com>
    
    * enhance test to verify non null doc doesnt match null query
    
    Signed-off-by: Surya Sashank Nistala <snistala@amazon.com>
    
    ---------
    
    Signed-off-by: Surya Sashank Nistala <snistala@amazon.com>

--- src/test/java/org/opensearch/securityanalytics/rules/backend/QueryBackendTests.java
+++ src/test/java/org/opensearch/securityanalytics/rules/backend/QueryBackendTests.java
@@ -282,7 +282,7 @@ public class QueryBackendTests extends OpenSearchTestCase {
                 "                sel:\n" +
                 "                    fieldA1: null\n" +
                 "                condition: sel", false));
-        Assert.assertEquals("mappedA: null", queries.get(0).toString());
+        Assert.assertEquals("mappedA: (NOT [* TO *])", queries.get(0).toString());
     }
 
     public void testConvertValueRegex() throws IOException, SigmaError {
@@ -525,7 +525,7 @@ public class QueryBackendTests extends OpenSearchTestCase {
                 "                        - value2\n" +
                 "                        - null\n" +
                 "                condition: sel", false));
-        Assert.assertEquals("(mappedA: \"value1\") OR (mappedA: \"value2\") OR (mappedA: null)", queries.get(0).toString());
+        Assert.assertEquals("(mappedA: \"value1\") OR (mappedA: \"value2\") OR (mappedA: (NOT [* TO *]))", queries.get(0).toString());
     }
 
     public void testConvertOrInListNumbers() throws IOException, SigmaError {
