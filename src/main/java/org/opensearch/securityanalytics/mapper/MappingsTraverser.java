4201f35c32d3004d097a254ad8c5e74dd810fcde
Megha Goyal <56077967+goyamegh@users.noreply.github.com>
2023-11-29 17:27:23 -0800

    #709 Fix merge conflicts while backporting
    Signed-off-by: Megha Goyal <goyamegh@amazon.com>

--- src/main/java/org/opensearch/securityanalytics/mapper/MappingsTraverser.java
+++ src/main/java/org/opensearch/securityanalytics/mapper/MappingsTraverser.java
@@ -15,9 +15,6 @@ import org.opensearch.common.xcontent.DeprecationHandler;
 import org.opensearch.common.xcontent.NamedXContentRegistry;
 import org.opensearch.common.xcontent.XContentParser;
 import org.opensearch.common.xcontent.json.JsonXContent;
-import org.opensearch.core.xcontent.DeprecationHandler;
-import org.opensearch.core.xcontent.NamedXContentRegistry;
-import org.opensearch.core.xcontent.XContentParser;
 
 import java.io.IOException;
 
