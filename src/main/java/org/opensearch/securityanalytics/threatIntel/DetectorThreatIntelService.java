2e90816d3f94b84b10e247782669c32e50d280c1
Joanne Wang <jowg@amazon.com>
2023-12-22 14:28:01 -0500

    DetectorThreatIntel only depends ThreatIntelService
    Signed-off-by: Joanne Wang <jowg@amazon.com>

--- src/main/java/org/opensearch/securityanalytics/threatIntel/DetectorThreatIntelService.java
+++ src/main/java/org/opensearch/securityanalytics/threatIntel/DetectorThreatIntelService.java
@@ -25,6 +25,7 @@ import org.opensearch.securityanalytics.action.SearchDetectorRequest;
 import org.opensearch.securityanalytics.model.Detector;
 import org.opensearch.securityanalytics.model.LogType;
 import org.opensearch.securityanalytics.model.ThreatIntelFeedData;
+import org.opensearch.securityanalytics.threatIntel.action.ThreatIntelService;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -46,12 +47,12 @@ public class DetectorThreatIntelService {
 
     private static final Logger log = LogManager.getLogger(DetectorThreatIntelService.class);
 
-    private final ThreatIntelFeedDataService threatIntelFeedDataService;
+    private final ThreatIntelService threatIntelService;
     private final Client client;
     private final NamedXContentRegistry xContentRegistry;
 
-    public DetectorThreatIntelService(ThreatIntelFeedDataService threatIntelFeedDataService, Client client, NamedXContentRegistry xContentRegistry) {
-        this.threatIntelFeedDataService = threatIntelFeedDataService;
+    public DetectorThreatIntelService(ThreatIntelService threatIntelService, Client client, NamedXContentRegistry xContentRegistry) {
+        this.threatIntelService = threatIntelService;
         this.client = client;
         this.xContentRegistry = xContentRegistry;
     }
@@ -123,7 +124,7 @@ public class DetectorThreatIntelService {
             }
 
             CountDownLatch latch = new CountDownLatch(1);
-            threatIntelFeedDataService.getThreatIntelFeedData(new ActionListener<>() {
+            threatIntelService.getThreatIntelFeedData(new ActionListener<>() {
                 @Override
                 public void onResponse(List<ThreatIntelFeedData> threatIntelFeedData) {
                     if (threatIntelFeedData.isEmpty()) {
--- src/main/java/org/opensearch/securityanalytics/threatIntel/action/ThreatIntelService.java
+++ src/main/java/org/opensearch/securityanalytics/threatIntel/action/ThreatIntelService.java
@@ -20,7 +20,9 @@ import org.opensearch.core.action.ActionListener;
 import org.opensearch.core.rest.RestStatus;
 import org.opensearch.index.engine.VersionConflictEngineException;
 import org.opensearch.jobscheduler.spi.LockModel;
+import org.opensearch.securityanalytics.model.ThreatIntelFeedData;
 import org.opensearch.securityanalytics.settings.SecurityAnalyticsSettings;
+import org.opensearch.securityanalytics.threatIntel.ThreatIntelFeedDataService;
 import org.opensearch.securityanalytics.threatIntel.common.TIFJobState;
 import org.opensearch.securityanalytics.threatIntel.common.TIFLockService;
 import org.opensearch.securityanalytics.threatIntel.jobscheduler.TIFJobParameter;
@@ -29,6 +31,7 @@ import org.opensearch.securityanalytics.threatIntel.jobscheduler.TIFJobUpdateSer
 
 import java.time.Instant;
 import java.util.ConcurrentModificationException;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static org.opensearch.securityanalytics.threatIntel.common.TIFLockService.LOCK_DURATION_IN_SECONDS;
@@ -46,6 +49,8 @@ public class ThreatIntelService {
     private final ClusterService clusterService;
     private final ClusterSettings clusterSettings;
 
+    private final ThreatIntelFeedDataService threatIntelFeedDataService;
+
 
     /**
      * Default constructor
@@ -57,37 +62,39 @@ public class ThreatIntelService {
     public ThreatIntelService(
             final TIFJobParameterService tifJobParameterService,
             final TIFJobUpdateService tifJobUpdateService,
+            final ThreatIntelFeedDataService threatIntelFeedDataService,
             final TIFLockService lockService,
             ClusterService clusterService
     ) {
         this.tifJobParameterService = tifJobParameterService;
         this.tifJobUpdateService = tifJobUpdateService;
+        this.threatIntelFeedDataService = threatIntelFeedDataService;
         this.lockService = lockService;
         this.clusterService = clusterService;
         this.clusterSettings = clusterService.getClusterSettings();
     }
 
-//    protected void doExecute(final Task task, final PutTIFJobRequest request, final ActionListener<AcknowledgedResponse> listener) {
-//        lockService.acquireLock(request.getName(), LOCK_DURATION_IN_SECONDS, ActionListener.wrap(lock -> {
-//            if (lock == null) {
-//                listener.onFailure(
-//                        new ConcurrentModificationException("another processor is holding a lock on the resource. Try again later")
-//                );
-//                log.error("another processor is a lock, BAD_REQUEST error", RestStatus.BAD_REQUEST);
-//                return;
-//            }
-//            try {
-//                internalDoExecute(request, lock, listener);
-//            } catch (Exception e) {
-//                lockService.releaseLock(lock);
-//                listener.onFailure(e);
-//                log.error("listener failed when executing", e);
-//            }
-//        }, exception -> {
-//            listener.onFailure(exception);
-//            log.error("execution failed", exception);
-//        }));
-//    }
+    protected void doExecute(final ActionListener<AcknowledgedResponse> listener) {
+        lockService.acquireLock("feed_updater", LOCK_DURATION_IN_SECONDS, ActionListener.wrap(lock -> {
+            if (lock == null) {
+                listener.onFailure(
+                        new ConcurrentModificationException("another processor is holding a lock on the resource. Try again later")
+                );
+                log.error("another processor is a lock, BAD_REQUEST error", RestStatus.BAD_REQUEST);
+                return;
+            }
+            try {
+                internalDoExecute(lock, listener);
+            } catch (Exception e) {
+                lockService.releaseLock(lock);
+                listener.onFailure(e);
+                log.error("listener failed when executing", e);
+            }
+        }, exception -> {
+            listener.onFailure(exception);
+            log.error("execution failed", exception);
+        }));
+    }
 
     /**
      * This method takes lock as a parameter and is responsible for releasing lock
@@ -177,6 +184,26 @@ public class ThreatIntelService {
             tifJobParameterService.updateJobSchedulerParameter(tifJobParameter, listener);
         } catch (Exception e) {
             log.error("Failed to mark tifJobParameter state as CREATE_FAILED for {}", tifJobParameter.getName(), e);
+        }
+    }
+
+    public void getThreatIntelFeedData(
+            ActionListener<List<ThreatIntelFeedData>> listener
+    ) {
+        String tifdIndex = threatIntelFeedDataService.getLatestIndexByCreationDate(); //gets the latest index
+        if (tifdIndex == null) {
+            doExecute(new ActionListener<AcknowledgedResponse>() {
+                @Override
+                public void onResponse(AcknowledgedResponse acknowledgedResponse) {
+                    threatIntelFeedDataService.getThreatIntelFeedData(listener);
+                }
+                @Override
+                public void onFailure(Exception e) {
+                    log.error("Failed to start TIF Job");
+                }
+            }); // create a new listener here
+        } else {
+            threatIntelFeedDataService.getThreatIntelFeedData(listener);
         }
     }
 }
