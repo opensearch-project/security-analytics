dd6e5e24a41718ba6d889eb113d970b405f94ed5
Joanne Wang <jowg@amazon.com>
2024-01-12 17:10:43 -0800

    Upgrade Lucene Codec to Lucene99 (#800)
    Signed-off-by: Joanne Wang <jowg@amazon.com>
    Co-authored-by: Andriy Redko <andriy.redko@aiven.io>
    (cherry picked from commit 988377a5f26cb7eedf13cb075a8e9624c29b78a8)

--- src/main/java/org/opensearch/securityanalytics/correlation/index/codec/CorrelationCodecVersion.java
+++ src/main/java/org/opensearch/securityanalytics/correlation/index/codec/CorrelationCodecVersion.java
@@ -5,10 +5,14 @@
 package org.opensearch.securityanalytics.correlation.index.codec;
 
 import org.apache.lucene.codecs.Codec;
-import org.apache.lucene.codecs.lucene95.Lucene95Codec;
+import org.apache.lucene.codecs.lucene99.Lucene99Codec;
+import org.apache.lucene.backward_codecs.lucene95.Lucene95Codec;
+import org.apache.lucene.codecs.perfield.PerFieldKnnVectorsFormat;
 import org.opensearch.index.mapper.MapperService;
-import org.opensearch.securityanalytics.correlation.index.codec.correlation950.CorrelationCodec;
-import org.opensearch.securityanalytics.correlation.index.codec.correlation950.PerFieldCorrelationVectorsFormat;
+import org.opensearch.securityanalytics.correlation.index.codec.correlation950.CorrelationCodec950;
+import org.opensearch.securityanalytics.correlation.index.codec.correlation990.CorrelationCodec990;
+import org.opensearch.securityanalytics.correlation.index.codec.correlation990.PerFieldCorrelationVectorsFormat990;
+import org.opensearch.securityanalytics.correlation.index.codec.correlation950.PerFieldCorrelationVectorsFormat950;
 
 import java.util.Optional;
 import java.util.function.BiFunction;
@@ -16,28 +20,35 @@ import java.util.function.Supplier;
 
 public enum CorrelationCodecVersion {
     V_9_5_0(
-            "CorrelationCodec",
+            "CorrelationCodec950",
             new Lucene95Codec(),
-            new PerFieldCorrelationVectorsFormat(Optional.empty()),
-            (userCodec, mapperService) -> new CorrelationCodec(userCodec, new PerFieldCorrelationVectorsFormat(Optional.of(mapperService))),
-            CorrelationCodec::new
+            new PerFieldCorrelationVectorsFormat950(Optional.empty()),
+            (userCodec, mapperService) -> new CorrelationCodec950(userCodec, new PerFieldCorrelationVectorsFormat950(Optional.of(mapperService))),
+            CorrelationCodec950::new
+    ),
+    V_9_9_0(
+            "CorrelationCodec990",
+            new Lucene99Codec(),
+            new PerFieldCorrelationVectorsFormat990(Optional.empty()),
+            (userCodec, mapperService) -> new CorrelationCodec990(userCodec, new PerFieldCorrelationVectorsFormat990(Optional.of(mapperService))),
+            CorrelationCodec990::new
     );
 
-    private static final CorrelationCodecVersion CURRENT = V_9_5_0;
+    private static final CorrelationCodecVersion CURRENT = V_9_9_0;
     private final String codecName;
     private final Codec defaultCodecDelegate;
-    private final PerFieldCorrelationVectorsFormat perFieldCorrelationVectorsFormat;
+    private final PerFieldKnnVectorsFormat perFieldKnnVectorsFormat;
     private final BiFunction<Codec, MapperService, Codec> correlationCodecSupplier;
     private final Supplier<Codec> defaultCorrelationCodecSupplier;
 
     CorrelationCodecVersion(String codecName,
                             Codec defaultCodecDelegate,
-                            PerFieldCorrelationVectorsFormat perFieldCorrelationVectorsFormat,
+                            PerFieldKnnVectorsFormat perFieldKnnVectorsFormat,
                             BiFunction<Codec, MapperService, Codec> correlationCodecSupplier,
                             Supplier<Codec> defaultCorrelationCodecSupplier) {
         this.codecName = codecName;
         this.defaultCodecDelegate = defaultCodecDelegate;
-        this.perFieldCorrelationVectorsFormat = perFieldCorrelationVectorsFormat;
+        this.perFieldKnnVectorsFormat = perFieldKnnVectorsFormat;
         this.correlationCodecSupplier = correlationCodecSupplier;
         this.defaultCorrelationCodecSupplier = defaultCorrelationCodecSupplier;
     }
@@ -50,8 +61,8 @@ public enum CorrelationCodecVersion {
         return defaultCodecDelegate;
     }
 
-    public PerFieldCorrelationVectorsFormat getPerFieldCorrelationVectorsFormat() {
-        return perFieldCorrelationVectorsFormat;
+    public PerFieldKnnVectorsFormat getPerFieldCorrelationVectorsFormat() {
+        return perFieldKnnVectorsFormat;
     }
 
     public BiFunction<Codec, MapperService, Codec> getCorrelationCodecSupplier() {
--- src/main/java/org/opensearch/securityanalytics/correlation/index/codec/correlation950/PerFieldCorrelationVectorsFormat950.java
+++ src/main/java/org/opensearch/securityanalytics/correlation/index/codec/correlation950/PerFieldCorrelationVectorsFormat950.java
@@ -4,15 +4,15 @@
  */
 package org.opensearch.securityanalytics.correlation.index.codec.correlation950;
 
-import org.apache.lucene.codecs.lucene95.Lucene95HnswVectorsFormat;
+import org.apache.lucene.backward_codecs.lucene95.Lucene95HnswVectorsFormat;
 import org.opensearch.index.mapper.MapperService;
 import org.opensearch.securityanalytics.correlation.index.codec.BasePerFieldCorrelationVectorsFormat;
 
 import java.util.Optional;
 
-public class PerFieldCorrelationVectorsFormat extends BasePerFieldCorrelationVectorsFormat {
+public class PerFieldCorrelationVectorsFormat950 extends BasePerFieldCorrelationVectorsFormat {
 
-    public PerFieldCorrelationVectorsFormat(final Optional<MapperService> mapperService) {
+    public PerFieldCorrelationVectorsFormat950(final Optional<MapperService> mapperService) {
         super(
                 mapperService,
                 Lucene95HnswVectorsFormat.DEFAULT_MAX_CONN,
