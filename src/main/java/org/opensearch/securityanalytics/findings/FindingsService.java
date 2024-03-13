c15e57728565c9a7d6726e077c89d4c462326513
Riya Saxena <riysaxen@amazon.com>
2024-03-13 11:49:04 -0700

    fix conflixt errors
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>

--- src/main/java/org/opensearch/securityanalytics/findings/FindingsService.java
+++ src/main/java/org/opensearch/securityanalytics/findings/FindingsService.java
@@ -147,6 +147,7 @@ public class FindingsService {
             ActionListener<GetFindingsResponse> listener
     ) {
         BoolQueryBuilder queryBuilder = getBoolQueryBuilder(detectionType, severity, findingIds, startTime, endTime);
+
         org.opensearch.commons.alerting.action.GetFindingsRequest req =
                 new org.opensearch.commons.alerting.action.GetFindingsRequest(
                 null,
