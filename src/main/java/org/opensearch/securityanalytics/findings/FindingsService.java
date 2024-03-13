7a63ec0517da0f7a83f178ad48999e8b034ab4ef
Riya Saxena <riysaxen@amazon.com>
2024-03-12 20:39:57 -0700

    remove unused imports

--- src/main/java/org/opensearch/securityanalytics/findings/FindingsService.java
+++ src/main/java/org/opensearch/securityanalytics/findings/FindingsService.java
@@ -15,7 +15,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.lucene.search.join.ScoreMode;
 import org.opensearch.OpenSearchStatusException;
-import org.opensearch.commons.alerting.util.CustomBoolQueryBuilder;
 import org.opensearch.core.action.ActionListener;
 import org.opensearch.client.Client;
 import org.opensearch.client.node.NodeClient;
