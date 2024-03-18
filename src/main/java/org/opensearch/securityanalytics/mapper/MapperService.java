23e989e9f104bb78f72c0a48bb1175d0e719feea
Riya Saxena <riysaxen@amazon.com>
2024-03-18 15:36:07 -0700

    removing threatIntel related code
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>

--- src/main/java/org/opensearch/securityanalytics/mapper/MapperService.java
+++ src/main/java/org/opensearch/securityanalytics/mapper/MapperService.java
@@ -535,7 +535,7 @@ public class MapperService {
                                 .collect(Collectors.toList());
 
                         actionListener.onResponse(
-                                new GetMappingsViewResponse(aliasMappings, unmappedIndexFields, filteredUnmappedFieldAliases, logTypeService.getIocFieldsList(logType))
+                                new GetMappingsViewResponse(aliasMappings, unmappedIndexFields, filteredUnmappedFieldAliases)
                         );
                     } catch (Exception e) {
                         actionListener.onFailure(e);
