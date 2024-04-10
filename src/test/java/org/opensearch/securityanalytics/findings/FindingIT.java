99b9b14387e266641b0fc1402179208bef438b0f
Chase Engelbrecht <engechas@amazon.com>
2024-04-10 09:40:39 -0700

    Add import
    Signed-off-by: Chase Engelbrecht <engechas@amazon.com>

--- src/test/java/org/opensearch/securityanalytics/findings/FindingIT.java
+++ src/test/java/org/opensearch/securityanalytics/findings/FindingIT.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import org.apache.http.HttpStatus;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.opensearch.client.Request;
 import org.opensearch.client.Response;
 import org.opensearch.client.ResponseException;
