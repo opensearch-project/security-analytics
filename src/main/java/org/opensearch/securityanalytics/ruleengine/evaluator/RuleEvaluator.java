9dbc8ca292d4c1a8fbd499f2e5fb49b31ad3cefa
Chase Engelbrecht <engechas@amazon.com>
2024-04-22 16:21:15 -0700

    Add javadoc for interfaces
    Signed-off-by: Chase Engelbrecht <engechas@amazon.com>

--- src/main/java/org/opensearch/securityanalytics/ruleengine/evaluator/RuleEvaluator.java
+++ src/main/java/org/opensearch/securityanalytics/ruleengine/evaluator/RuleEvaluator.java
@@ -5,5 +5,11 @@ import org.opensearch.securityanalytics.ruleengine.model.Match;
 import java.util.List;
 
 public interface RuleEvaluator<T> {
+    /**
+     * A method to evaluate the rules against a set of incoming data.
+     *
+     * @param data - the data to be evaluated against the rules
+     * @return - A list of Matches for positive rule evaluations against the incoming data
+     */
     List<Match> evaluate(List<T> data);
 }
--- src/main/java/org/opensearch/securityanalytics/ruleengine/provider/RuleProvider.java
+++ src/main/java/org/opensearch/securityanalytics/ruleengine/provider/RuleProvider.java
@@ -3,5 +3,10 @@ package org.opensearch.securityanalytics.ruleengine.provider;
 import java.util.List;
 
 public interface RuleProvider {
+    /**
+     * A method to fetch RuleData from an external source
+     *
+     * @return - A list of RuleData used to parse the rules into the internal representation used for evaluation
+     */
     List<RuleData> getRuleData();
 }
