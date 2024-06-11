ff38749353c3d440f0b491bb138d1528bd300e4a
Riya Saxena <riysaxen@amazon.com>
2024-06-11 16:20:06 -0700

    license headers added
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>

--- src/main/java/org/opensearch/securityanalytics/action/GetCorrelationAlertsAction.java
+++ src/main/java/org/opensearch/securityanalytics/action/GetCorrelationAlertsAction.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package org.opensearch.securityanalytics.action;
 
 import org.opensearch.action.ActionType;
--- src/main/java/org/opensearch/securityanalytics/resthandler/RestAcknowledgeCorrelationAlertsAction.java
+++ src/main/java/org/opensearch/securityanalytics/resthandler/RestAcknowledgeCorrelationAlertsAction.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package org.opensearch.securityanalytics.resthandler;
 
 import org.opensearch.client.node.NodeClient;
