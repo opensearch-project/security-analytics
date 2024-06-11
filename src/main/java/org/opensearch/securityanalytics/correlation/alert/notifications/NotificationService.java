62e4453dde8977a43c16c7d7b27ffc4413ec00df
Riya <69919272+riysaxen-amzn@users.noreply.github.com>
2024-06-11 14:27:41 -0700

    Alerts in correlations [Experminental] (#1040)
    * notification for alerting in correlation
    
    * correlation alerts mapping change
    
    * working code
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * alertsInCorrelation without notifciations
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * alertsInCorrelation without notifciations
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * alertsInCorrelation without notifciations
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * alerts in correlations notification service added
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * addressing the comments
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * addressing the comments
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * address the design changes discussed
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * address the design changes discussed
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * fixed tests
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    ---------
    
    Signed-off-by: Riya <69919272+riysaxen-amzn@users.noreply.github.com>
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>

---
+++ src/main/java/org/opensearch/securityanalytics/correlation/alert/notifications/NotificationService.java
@@ -0,0 +1,112 @@
+package org.opensearch.securityanalytics.correlation.alert.notifications;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.opensearch.client.node.NodeClient;
+import org.opensearch.commons.notifications.NotificationsPluginInterface;
+import org.opensearch.commons.notifications.action.*;
+import org.opensearch.commons.notifications.model.ChannelMessage;
+import org.opensearch.commons.notifications.model.EventSource;
+import org.opensearch.commons.notifications.model.SeverityType;
+import org.opensearch.commons.notifications.model.NotificationConfigInfo;
+import org.opensearch.commons.notifications.action.GetNotificationConfigRequest;
+import org.opensearch.commons.notifications.action.GetNotificationConfigResponse;
+import org.opensearch.core.action.ActionListener;
+import org.opensearch.core.rest.RestStatus;
+import org.opensearch.securityanalytics.util.SecurityAnalyticsException;
+import org.opensearch.script.ScriptService;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Collections;
+import org.opensearch.script.Script;
+import org.opensearch.script.TemplateScript;
+import org.opensearch.commons.notifications.model.SeverityType;
+
+public class NotificationService {
+
+    private static final Logger logger = LogManager.getLogger(NotificationService.class);
+
+    private static ScriptService scriptService;
+    private final NodeClient client;
+
+    public NotificationService(NodeClient client, ScriptService scriptService) {
+        this.client = client;
+        this.scriptService = scriptService;
+    }
+    /**
+     * Extension function for publishing a notification to a channel in the Notification plugin.
+     */
+    public void sendNotification(String configId, String severity, String subject, String notificationMessageText) throws IOException {
+        ChannelMessage message = generateMessage(notificationMessageText);
+        List<String> channelIds = new ArrayList<>();
+        channelIds.add(configId);
+        SeverityType severityType = SeverityType.Companion.fromTagOrDefault(severity);
+        NotificationsPluginInterface.INSTANCE.sendNotification(client, new EventSource(subject, configId, severityType, Collections.emptyList()), message, channelIds, new ActionListener<SendNotificationResponse>() {
+            @Override
+            public void onResponse(SendNotificationResponse sendNotificationResponse) {
+                if(sendNotificationResponse.getStatus() == RestStatus.OK) {
+                    logger.info("Successfully sent a notification, Notification Event: " + sendNotificationResponse.getNotificationEvent());
+                }
+                else {
+                    logger.error("Error while sending a notification, Notification Event: " + sendNotificationResponse.getNotificationEvent());
+                }
+
+            }
+            @Override
+            public void onFailure(Exception e) {
+                logger.error("Failed while sending a notification with " + configId, e);
+            }
+        });
+    }
+
+    /**
+     * Gets a NotificationConfigInfo object by ID if it exists.
+     */
+    public GetNotificationConfigResponse getNotificationConfigInfo(String id) {
+
+        Set idSet = new HashSet<String>();
+        idSet.add(id);
+        GetNotificationConfigRequest getNotificationConfigRequest = new GetNotificationConfigRequest(idSet, 0, 10, null, null, new HashMap<>());
+        GetNotificationConfigResponse configResp = null;
+        NotificationsPluginInterface.INSTANCE.getNotificationConfig(client, getNotificationConfigRequest, new ActionListener<GetNotificationConfigResponse>() {
+            @Override
+            public void onResponse(GetNotificationConfigResponse getNotificationConfigResponse) {
+                if (getNotificationConfigResponse.getStatus() == RestStatus.OK) {
+                    getNotificationConfigResponse = configResp;
+                } else {
+                    logger.error("Successfully sent a notification, Notification Event: " + getNotificationConfigResponse);
+                }
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                logger.error("Notification config [" + id + "] was not found");
+                new SecurityAnalyticsException("Failed to fetch notification config", RestStatus.INTERNAL_SERVER_ERROR, e);
+            }
+        });
+        logger.info("Notification config response is: {} ", configResp);
+        return configResp;
+    }
+
+    public static ChannelMessage generateMessage(String message) {
+        return new ChannelMessage(
+                message,
+                null,
+                null
+        );
+    }
+
+    public static String compileTemplate(CorrelationAlertContext ctx, Script template) {
+        TemplateScript.Factory factory = scriptService.compile(template, TemplateScript.CONTEXT);
+        Map<String, Object> params = new HashMap<>(template.getParams());
+        params.put("ctx", ctx.asTemplateArg());
+        TemplateScript templateScript = factory.newInstance(params);
+        return templateScript.execute();
+    }
+
+}
