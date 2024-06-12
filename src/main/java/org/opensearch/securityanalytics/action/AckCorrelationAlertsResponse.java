a74f509ccbac51531b77c371550b81fcd867931a
Riya <69919272+riysaxen-amzn@users.noreply.github.com>
2024-06-11 17:24:55 -0700

    Alerts in Correlations Part 2 (#1062)
    * notification for alerting in correlation
    
    * correlation alerts mapping change
    
    * working code
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * alertsInCorrelation without notifciations
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * alertsInCorrelation without notifciations
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * alertsInCorrelation without notifciations
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * alerts in correlations notification service added
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * addressing the comments
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * addressing the comments
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * getCorrelationAlerts API changes
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * APIs added for Alerts in Correlations
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * update alerts with an errorMessage  when correlationRule is deleted
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * address the design changes discussed
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * address the design changes discussed
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * fixed tests
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * minor fixes due to merge
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * alerts API changes
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * klint fixes
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * license headers added
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * fixed format violations
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    ---------
    
    Signed-off-by: Riya <69919272+riysaxen-amzn@users.noreply.github.com>
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>

---
+++ src/main/java/org/opensearch/securityanalytics/action/AckCorrelationAlertsResponse.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.opensearch.securityanalytics.action;
+
+import org.opensearch.commons.alerting.model.CorrelationAlert;
+import org.opensearch.core.action.ActionResponse;
+import org.opensearch.core.common.io.stream.StreamInput;
+import org.opensearch.core.common.io.stream.StreamOutput;
+import org.opensearch.core.xcontent.ToXContentObject;
+import org.opensearch.core.xcontent.XContentBuilder;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+public class AckCorrelationAlertsResponse extends ActionResponse implements ToXContentObject {
+
+    private final List<CorrelationAlert> acknowledged;
+    private final List<CorrelationAlert> failed;
+
+    public AckCorrelationAlertsResponse(List<CorrelationAlert> acknowledged, List<CorrelationAlert> failed) {
+        this.acknowledged = acknowledged;
+        this.failed = failed;
+    }
+
+    public AckCorrelationAlertsResponse(StreamInput sin) throws IOException {
+        this(
+                Collections.unmodifiableList(sin.readList(CorrelationAlert::new)),
+                Collections.unmodifiableList(sin.readList(CorrelationAlert::new))
+        );
+    }
+
+    @Override
+    public void writeTo(StreamOutput streamOutput) throws IOException {
+        streamOutput.writeList(this.acknowledged);
+        streamOutput.writeList(this.failed);
+    }
+
+    @Override
+    public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+        builder.startObject()
+                .field("acknowledged",this.acknowledged)
+                .field("failed",this.failed);
+        return builder.endObject();
+    }
+
+    public List<CorrelationAlert> getAcknowledged() {
+        return acknowledged;
+    }
+
+    public List<CorrelationAlert> getFailed() {
+        return failed;
+    }
+}
