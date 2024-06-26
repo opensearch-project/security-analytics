24d05d191bee465ba1beb403c81b4916a63da6f0
Joanne Wang <jowg@amazon.com>
2024-06-26 10:24:41 -0700

    fix the rollover name
    Signed-off-by: Joanne Wang <jowg@amazon.com>

--- src/main/java/org/opensearch/securityanalytics/services/STIX2IOCFeedStore.java
+++ src/main/java/org/opensearch/securityanalytics/services/STIX2IOCFeedStore.java
@@ -56,7 +56,8 @@ public class STIX2IOCFeedStore implements FeedStore {
     public static final String IOC_FEED_ID_PLACEHOLDER = "FEED_ID";
     public static final String IOC_INDEX_NAME_TEMPLATE = IOC_INDEX_NAME_BASE + "-" + IOC_FEED_ID_PLACEHOLDER;
     public static final String IOC_WRITE_INDEX_ALIAS = IOC_INDEX_NAME_TEMPLATE;
-    public static final String IOC_INDEX_PATTERN = "<" + IOC_INDEX_NAME_TEMPLATE + "-" + Instant.now().toEpochMilli() +"-000001>"; // TODO: change this
+    public static final String IOC_TIME_PLACEHOLDER = "TIME";
+    public static final String IOC_INDEX_PATTERN = IOC_INDEX_NAME_TEMPLATE + "-" + IOC_TIME_PLACEHOLDER;
 
     private final Logger log = LogManager.getLogger(STIX2IOCFeedStore.class);
     Instant startTime = Instant.now();
@@ -160,9 +161,9 @@ public class STIX2IOCFeedStore implements FeedStore {
             listener.onFailure(new OpenSearchException("Alias not initialized"));
             return;
         }
-        // We have to pass null for newIndexName in order to get Elastic to increment the alias count.
-        RolloverRequest request = new RolloverRequest(alias, null); // TODO: change name here
-        request.getCreateIndexRequest().index(pattern) // todo: remove pattern
+
+        RolloverRequest request = new RolloverRequest(alias, pattern);
+        request.getCreateIndexRequest()
                 .mapping(iocIndexMapping())
                 .settings(Settings.builder().put("index.hidden", true).build());
         client.admin().indices().rolloverIndex(
@@ -242,7 +243,9 @@ public class STIX2IOCFeedStore implements FeedStore {
     }
 
     public static String getIocIndexRolloverPattern(String feedSourceConfigId) {
-        return IOC_INDEX_PATTERN.replace(IOC_FEED_ID_PLACEHOLDER, feedSourceConfigId.toLowerCase(Locale.ROOT));
+        return IOC_INDEX_PATTERN
+                .replace(IOC_FEED_ID_PLACEHOLDER, feedSourceConfigId.toLowerCase(Locale.ROOT))
+                .replace(IOC_TIME_PLACEHOLDER, Long.toString(Instant.now().toEpochMilli()));
     }
 
 
