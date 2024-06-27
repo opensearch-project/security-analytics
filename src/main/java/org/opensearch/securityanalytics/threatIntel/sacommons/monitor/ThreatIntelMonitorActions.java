adc305bef9b694375a7017233cb4333d1952ae59
Joanne Wang <jowg@amazon.com>
2024-06-27 16:21:05 -0700

    fix action names
    Signed-off-by: Joanne Wang <jowg@amazon.com>

--- src/main/java/org/opensearch/securityanalytics/threatIntel/sacommons/monitor/ThreatIntelMonitorActions.java
+++ src/main/java/org/opensearch/securityanalytics/threatIntel/sacommons/monitor/ThreatIntelMonitorActions.java
@@ -1,9 +1,9 @@
 package org.opensearch.securityanalytics.threatIntel.sacommons.monitor;
 
 public class ThreatIntelMonitorActions {
-    public static final String INDEX_THREAT_INTEL_MONITOR_ACTION_NAME = "cluster:admin/security_analytics/threatIntel/monitor/write";
-    public static final String SEARCH_THREAT_INTEL_MONITOR_ACTION_NAME = "cluster:admin/security_analytics/threatIntel/monitor/search";
-    public static final String DELETE_THREAT_INTEL_MONITOR_ACTION_NAME = "cluster:admin/security_analytics/threatIntel/monitor/delete";
-    public static final String GET_THREAT_INTEL_ALERTS_ACTION_NAME = "cluster:admin/security_analytics/threatIntel/alerts/get";
-    public static final String UPDATE_THREAT_INTEL_ALERT_STATUS_ACTION_NAME = "cluster:admin/security_analytics/threatIntel/alerts/status/update";
+    public static final String INDEX_THREAT_INTEL_MONITOR_ACTION_NAME = "cluster:admin/opensearch/securityanalytics/threatIntel/monitors/write";
+    public static final String SEARCH_THREAT_INTEL_MONITOR_ACTION_NAME = "cluster:admin/opensearch/securityanalytics/threatIntel/monitors/search";
+    public static final String DELETE_THREAT_INTEL_MONITOR_ACTION_NAME = "cluster:admin/opensearch/securityanalytics/threatIntel/monitors/delete";
+    public static final String GET_THREAT_INTEL_ALERTS_ACTION_NAME = "cluster:admin/opensearch/securityanalytics/threatIntel/alerts/get";
+    public static final String UPDATE_THREAT_INTEL_ALERT_STATUS_ACTION_NAME = "cluster:admin/opensearch/securityanalytics/threatIntel/alerts/status/update";
 }
