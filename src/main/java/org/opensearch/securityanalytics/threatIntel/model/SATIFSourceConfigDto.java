f3039d74ad632b9f54838f3f4f88bd2170bbab86
Joanne Wang <jowg@amazon.com>
2024-06-27 17:46:43 -0700

    Fix validation for source types (#1108)
    * fix validation
    
    Signed-off-by: Joanne Wang <jowg@amazon.com>
    
    * switch case
    
    Signed-off-by: Joanne Wang <jowg@amazon.com>
    
    ---------
    
    Signed-off-by: Joanne Wang <jowg@amazon.com>

--- src/main/java/org/opensearch/securityanalytics/threatIntel/model/SATIFSourceConfigDto.java
+++ src/main/java/org/opensearch/securityanalytics/threatIntel/model/SATIFSourceConfigDto.java
@@ -454,20 +454,26 @@ public class SATIFSourceConfigDto implements Writeable, ToXContentObject, TIFSou
 
     private static void validateSourceConfigDto(SourceConfigType sourceConfigType, Boolean isEnabled, Source source, Schedule schedule) {
         // validate source config dto
-        if (sourceConfigType.equals(SourceConfigType.IOC_UPLOAD)) {
-            if (isEnabled == true) {
-                throw new IllegalArgumentException("Job Scheduler cannot be enabled for file_upload type");
-            }
-            if (schedule != null) {
-                throw new IllegalArgumentException("Cannot pass in schedule for a file_upload type");
-            }
-        } else if (sourceConfigType.equals(SourceConfigType.S3_CUSTOM)) {
-            if (source == null) {
-                throw new IllegalArgumentException("Must pass in source for a s3_custom type");
-            }
-            if (schedule == null) {
-                throw new IllegalArgumentException("Must pass in schedule for a s3_custom type");
-            }
+        switch (sourceConfigType) {
+            case IOC_UPLOAD:
+                if (isEnabled == true) {
+                    throw new IllegalArgumentException("Job Scheduler cannot be enabled for IOC_UPLOAD type");
+                }
+                if (schedule != null) {
+                    throw new IllegalArgumentException("Cannot pass in schedule for IOC_UPLOAD type");
+                }
+                if (source != null && source instanceof IocUploadSource == false) {
+                    throw new IllegalArgumentException("Source must be IOC_UPLOAD type");
+                }
+                break;
+            case S3_CUSTOM:
+                if (schedule == null) {
+                    throw new IllegalArgumentException("Must pass in schedule for S3_CUSTOM type");
+                }
+                if (source != null && source instanceof S3Source == false) {
+                    throw new IllegalArgumentException("Source must be S3_CUSTOM type");
+                }
+                break;
         }
     }
 
