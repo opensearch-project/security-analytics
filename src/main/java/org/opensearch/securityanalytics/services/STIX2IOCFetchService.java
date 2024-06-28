b5adadd901b5b83bb45763db447b1f2a2cad7941
AWSHurneyt <hurneyt@amazon.com>
2024-06-28 16:32:38 -0700

    Refactored calls to S3. Added support for consuming security analytics commons directly from project rootDir. (#1114)
    * Fixed validation bug.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Fixed comment.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Implemented support for making calls to S3 using either S3Client, or AmazonS3. Dependency on S3Client will eventually be removed.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Refactored build.gradle to consume SA commons from jar in root directory.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Updated jar.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    ---------
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>

--- src/main/java/org/opensearch/securityanalytics/services/STIX2IOCFetchService.java
+++ src/main/java/org/opensearch/securityanalytics/services/STIX2IOCFetchService.java
@@ -5,6 +5,8 @@
 
 package org.opensearch.securityanalytics.services;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.opensearch.client.Client;
@@ -13,8 +15,10 @@ import org.opensearch.core.action.ActionListener;
 import org.opensearch.core.action.ActionResponse;
 import org.opensearch.core.common.io.stream.StreamInput;
 import org.opensearch.core.common.io.stream.StreamOutput;
+import org.opensearch.core.rest.RestStatus;
 import org.opensearch.core.xcontent.ToXContentObject;
 import org.opensearch.core.xcontent.XContentBuilder;
+import org.opensearch.securityanalytics.action.TestS3ConnectionResponse;
 import org.opensearch.securityanalytics.commons.connector.Connector;
 import org.opensearch.securityanalytics.commons.connector.S3Connector;
 import org.opensearch.securityanalytics.commons.connector.factory.InputCodecFactory;
@@ -32,11 +36,22 @@ import org.opensearch.securityanalytics.model.STIX2IOCDto;
 import org.opensearch.securityanalytics.settings.SecurityAnalyticsSettings;
 import org.opensearch.securityanalytics.threatIntel.model.S3Source;
 import org.opensearch.securityanalytics.threatIntel.model.SATIFSourceConfig;
+import org.opensearch.securityanalytics.threatIntel.service.TIFJobParameterService;
+import org.opensearch.securityanalytics.util.SecurityAnalyticsException;
+import software.amazon.awssdk.core.exception.SdkException;
 import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.sts.model.StsException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * IOC Service implements operations that interact with retrieving IOCs from data sources,
@@ -44,6 +59,7 @@ import java.util.List;
  */
 public class STIX2IOCFetchService {
     private final Logger log = LogManager.getLogger(STIX2IOCFetchService.class);
+    private final String ENDPOINT_CONFIG_PATH = "/threatIntelFeed/internalAuthEndpoint.txt";
 
     private Client client;
     private ClusterService clusterService;
@@ -52,14 +68,16 @@ public class STIX2IOCFetchService {
 
     // TODO hurneyt this is using TIF batch size setting. Consider adding IOC-specific setting
     private Integer batchSize;
+    private String internalAuthEndpoint = "";
 
     public STIX2IOCFetchService(Client client, ClusterService clusterService) {
         this.client = client;
         this.clusterService = clusterService;
+        this.internalAuthEndpoint = getEndpoint();
 
         StsAssumeRoleCredentialsProviderFactory factory =
                 new StsAssumeRoleCredentialsProviderFactory(new StsClientFactory());
-        s3ClientFactory = new S3ClientFactory(factory);
+        s3ClientFactory = new S3ClientFactory(factory, internalAuthEndpoint);
         connectorFactory = new STIX2IOCConnectorFactory(new InputCodecFactory(), s3ClientFactory);
         batchSize = clusterService.getClusterSettings().get(SecurityAnalyticsSettings.BATCH_SIZE);
     }
@@ -104,14 +122,75 @@ public class STIX2IOCFetchService {
         }
     }
 
-    public HeadObjectResponse testS3Connection(S3ConnectorConfig s3ConnectorConfig) {
-        S3Connector<STIX2> connector = (S3Connector<STIX2>) constructS3Connector(s3ConnectorConfig);
-        return connector.testS3Connection(s3ConnectorConfig);
+    public void testS3Connection(S3ConnectorConfig s3ConnectorConfig, ActionListener<TestS3ConnectionResponse> listener) {
+        if (internalAuthEndpoint.isEmpty()) {
+            testS3ClientConnection(s3ConnectorConfig, listener);
+        } else {
+            testAmazonS3Connection(s3ConnectorConfig, listener);
+        }
+    }
+
+    private void testS3ClientConnection(S3ConnectorConfig s3ConnectorConfig, ActionListener<TestS3ConnectionResponse> listener) {
+        try {
+            S3Connector<STIX2> connector = (S3Connector<STIX2>) constructS3Connector(s3ConnectorConfig);
+            HeadObjectResponse response = connector.testS3Connection(s3ConnectorConfig);
+            listener.onResponse(new TestS3ConnectionResponse(RestStatus.fromCode(response.sdkHttpResponse().statusCode()), ""));
+        } catch (NoSuchKeyException noSuchKeyException) {
+            log.warn("S3Client connection test failed with NoSuchKeyException: ", noSuchKeyException);
+            listener.onResponse(new TestS3ConnectionResponse(RestStatus.fromCode(noSuchKeyException.statusCode()), noSuchKeyException.awsErrorDetails().errorMessage()));
+        } catch (S3Exception s3Exception) {
+            log.warn("S3Client connection test failed with S3Exception: ", s3Exception);
+            listener.onResponse(new TestS3ConnectionResponse(RestStatus.fromCode(s3Exception.statusCode()), "Resource not found."));
+        } catch (StsException stsException) {
+            log.warn("S3Client connection test failed with StsException: ", stsException);
+            listener.onResponse(new TestS3ConnectionResponse(RestStatus.fromCode(stsException.statusCode()), stsException.awsErrorDetails().errorMessage()));
+        } catch (SdkException sdkException ) {
+            // SdkException is a RunTimeException that doesn't have a status code.
+            // Logging the full exception, and providing generic response as output.
+            log.warn("S3Client connection test failed with SdkException: ", sdkException);
+            listener.onResponse(new TestS3ConnectionResponse(RestStatus.FORBIDDEN, "Resource not found."));
+        } catch (Exception e) {
+            log.warn("S3Client connection test failed with error: ", e);
+            listener.onFailure(SecurityAnalyticsException.wrap(e));
+        }
+
+    }
+
+    private void testAmazonS3Connection(S3ConnectorConfig s3ConnectorConfig, ActionListener<TestS3ConnectionResponse> listener) {
+        try {
+            S3Connector<STIX2> connector = (S3Connector<STIX2>) constructS3Connector(s3ConnectorConfig);
+            boolean response = connector.testAmazonS3Connection(s3ConnectorConfig);
+            listener.onResponse(new TestS3ConnectionResponse(response ? RestStatus.OK : RestStatus.FORBIDDEN, ""));
+        } catch (AmazonServiceException e) {
+            log.warn("AmazonS3 connection test failed with AmazonServiceException: ", e);
+            listener.onResponse(new TestS3ConnectionResponse(RestStatus.fromCode(e.getStatusCode()), e.getErrorMessage()));
+        } catch (SdkClientException e) {
+            // SdkException is a RunTimeException that doesn't have a status code.
+            // Logging the full exception, and providing generic response as output.
+            log.warn("AmazonS3 connection test failed with SdkClientException: ", e);
+            listener.onResponse(new TestS3ConnectionResponse(RestStatus.FORBIDDEN, "Resource not found."));
+        } catch (Exception e) {
+            log.warn("AmazonS3 connection test failed with error: ", e);
+            listener.onFailure(SecurityAnalyticsException.wrap(e));
+        }
     }
 
     private Connector<STIX2> constructS3Connector(S3ConnectorConfig s3ConnectorConfig) {
         FeedConfiguration feedConfiguration = new FeedConfiguration(IOCSchema.STIX2, InputCodecSchema.ND_JSON, s3ConnectorConfig);
+        if (internalAuthEndpoint.isEmpty()) {
+            return constructS3ClientConnector(feedConfiguration);
+        } else {
+            return constructAmazonS3Connector(feedConfiguration);
+        }
+    }
+
+    private Connector<STIX2> constructS3ClientConnector(FeedConfiguration feedConfiguration) {
         return connectorFactory.doCreate(feedConfiguration);
+    }
+
+    private Connector<STIX2> constructAmazonS3Connector(FeedConfiguration feedConfiguration) {
+        List<String> clusterTuple = List.of(clusterService.getClusterName().value().split(":"));
+        return connectorFactory.createAmazonS3Connector(feedConfiguration, clusterTuple);
     }
 
     private S3ConnectorConfig constructS3ConnectorConfig(SATIFSourceConfig saTifSourceConfig) {
@@ -133,6 +212,19 @@ public class STIX2IOCFetchService {
         if (s3ConnectorConfig.getRegion() == null || s3ConnectorConfig.getRegion().isEmpty()) {
             throw new IllegalArgumentException("Region is required.");
         }
+    }
+
+    private String getEndpoint() {
+        try {
+            try (InputStream is = TIFJobParameterService.class.getResourceAsStream(ENDPOINT_CONFIG_PATH)) {
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
+                    return reader.lines().map(String::trim).collect(Collectors.joining());
+                }
+            }
+        } catch (Exception e) {
+            log.debug(String.format("Resource file [%s] doesn't exist.", ENDPOINT_CONFIG_PATH));
+        }
+        return "";
     }
 
     public static class STIX2IOCFetchResponse extends ActionResponse implements ToXContentObject {
@@ -161,8 +253,6 @@ public class STIX2IOCFetchService {
         @Override
         public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
             return builder.startObject()
-                    // TODO hurneyt include IOCs in response?
-//                    .field(IOCS_FIELD, this.iocs)
                     .field(TOTAL_FIELD, iocs.size())
                     .field(DURATION_FIELD, duration)
                     .endObject();
