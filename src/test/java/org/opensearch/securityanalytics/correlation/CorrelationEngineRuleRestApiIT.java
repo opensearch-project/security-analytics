644dab8d6ebdf63a035061df7a3b7884ca6ae512
Riya <69919272+riysaxen-amzn@users.noreply.github.com>
2024-07-06 22:41:34 -0700

    added correlationAlert integ tests (#1099) (#1141)
    * added correlationAlert integ tests
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * added licences
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * fixed imports
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    * deleted SecureCorrelationAlerts Tests, will add later
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    
    ---------
    
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>
    (cherry picked from commit e8d78790ffbf344a68efd05417c8af2469948f57)

--- src/test/java/org/opensearch/securityanalytics/correlation/CorrelationEngineRuleRestApiIT.java
+++ src/test/java/org/opensearch/securityanalytics/correlation/CorrelationEngineRuleRestApiIT.java
@@ -15,9 +15,11 @@ import org.opensearch.securityanalytics.model.CorrelationRule;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import static org.opensearch.securityanalytics.TestHelpers.randomCorrelationRule;
+import static org.opensearch.securityanalytics.TestHelpers.randomCorrelationRuleWithTrigger;
 
 public class CorrelationEngineRuleRestApiIT extends SecurityAnalyticsRestTestCase {
 
@@ -112,5 +114,43 @@ public class CorrelationEngineRuleRestApiIT extends SecurityAnalyticsRestTestCas
         response = makeRequest(client(), "POST", SecurityAnalyticsPlugin.CORRELATION_RULES_BASE_URI + "/_search", Collections.emptyMap(), new StringEntity(request), new BasicHeader("Content-type", "application/json"));
         responseMap = responseAsMap(response);
         Assert.assertEquals(1, Integer.parseInt(((Map<String, Object>) ((Map<String, Object>) responseMap.get("hits")).get("total")).get("value").toString()));
+    }
+
+    public void testSearchCorrelationRuleWithTrigger() throws IOException {
+        CorrelationRule rule = randomCorrelationRuleWithTrigger("custom-rule");
+        Response response = makeRequest(client(), "POST", SecurityAnalyticsPlugin.CORRELATION_RULES_BASE_URI, Collections.emptyMap(), toHttpEntity(rule));
+        Assert.assertEquals(201, response.getStatusLine().getStatusCode());
+        Map<String, Object> responseMap = responseAsMap(response);
+        Assert.assertEquals("custom-rule", ((Map<String, Object>) responseMap.get("rule")).get("name"));
+
+        String request = "{\n" +
+                "  \"query\": {\n" +
+                "    \"nested\": {\n" +
+                "      \"path\": \"correlate\",\n" +
+                "      \"query\": {\n" +
+                "        \"bool\": {\n" +
+                "          \"must\": [\n" +
+                "            { \"match\": {\"correlate.category\": \"network\"}}\n" +
+                "          ]\n" +
+                "        }\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+        response = makeRequest(client(), "POST", SecurityAnalyticsPlugin.CORRELATION_RULES_BASE_URI + "/_search", Collections.emptyMap(), new StringEntity(request), new BasicHeader("Content-type", "application/json"));
+        responseMap = responseAsMap(response);
+        // Assuming the hits contain the matched documents
+        Map<String, Object> hits = (Map<String, Object>) responseMap.get("hits");
+        Assert.assertNotNull(hits);
+
+        List<Map<String, Object>> hitsList = (List<Map<String, Object>>) hits.get("hits");
+        Assert.assertEquals(1, hitsList.size()); // Assuming you expect exactly one hit
+
+        Map<String, Object> hit = hitsList.get(0);
+        Map<String, Object> source = (Map<String, Object>) hit.get("_source");
+        Assert.assertNotNull(source);
+
+        Object trigger = source.get("trigger");
+        Assert.assertNotNull(trigger);
     }
 }
