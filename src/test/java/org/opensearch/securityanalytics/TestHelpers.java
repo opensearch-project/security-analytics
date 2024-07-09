3be48288b55ee0ef6fde1833566a07bd428a9b4a
Joanne Wang <jowg@amazon.com>
2024-07-09 13:15:10 -0400

    Changes threat intel default store config model (#1133)
    * change store config model
    
    Signed-off-by: Joanne Wang <jowg@amazon.com>
    
    * add validation to ioc type enum name
    
    Signed-off-by: Joanne Wang <jowg@amazon.com>
    
    * change alias to index pattern
    
    Signed-off-by: Joanne Wang <jowg@amazon.com>
    
    * make variables final
    
    Signed-off-by: Joanne Wang <jowg@amazon.com>
    
    * ensure ioc types are part of ioc_types
    
    Signed-off-by: Joanne Wang <jowg@amazon.com>
    
    * fix integ test
    
    Signed-off-by: Joanne Wang <jowg@amazon.com>
    
    * remove alias and rename active index
    
    Signed-off-by: Joanne Wang <jowg@amazon.com>
    
    * fix test
    
    Signed-off-by: Joanne Wang <jowg@amazon.com>
    
    * add enabled for scan flag in source config.
    
    Signed-off-by: Surya Sashank Nistala <snistala@amazon.com>
    
    ---------
    
    Signed-off-by: Joanne Wang <jowg@amazon.com>
    Signed-off-by: Surya Sashank Nistala <snistala@amazon.com>
    Co-authored-by: Surya Sashank Nistala <snistala@amazon.com>

--- src/test/java/org/opensearch/securityanalytics/TestHelpers.java
+++ src/test/java/org/opensearch/securityanalytics/TestHelpers.java
@@ -248,7 +248,7 @@ public class TestHelpers {
     }
 
     public static CorrelationRule randomCorrelationRuleWithTrigger(String name) {
-        name = name.isEmpty()? "><script>prompt(document.domain)</script>": name;
+        name = name.isEmpty() ? "><script>prompt(document.domain)</script>" : name;
         List<Action> actions = new ArrayList<Action>();
         CorrelationRuleTrigger trigger = new CorrelationRuleTrigger("trigger-123", "Trigger 1", "high", actions);
         return new CorrelationRule(CorrelationRule.NO_ID, CorrelationRule.NO_VERSION, name,
@@ -2825,7 +2825,8 @@ public class TestHelpers {
                 lastRefreshedTime,
                 lastRefreshedUser,
                 isEnabled,
-                iocTypes
+                iocTypes,
+                true
         );
     }
 
@@ -2889,9 +2890,7 @@ public class TestHelpers {
             schedule = new org.opensearch.jobscheduler.spi.schedule.IntervalSchedule(Instant.now(), 1, ChronoUnit.DAYS);
         }
         if (iocStoreConfig == null) {
-            Map<String, List<String>> iocMapStore = new HashMap<>();
-            iocMapStore.put("ip", List.of("index_name"));
-            iocStoreConfig = new DefaultIocStoreConfig(iocMapStore);
+            iocStoreConfig = new DefaultIocStoreConfig(List.of(new DefaultIocStoreConfig.IocToIndexDetails(IOCType.domain_name, "indexPattern", "writeIndex")));
         }
         if (iocTypes == null) {
             iocTypes = List.of("ip");
@@ -2916,7 +2915,8 @@ public class TestHelpers {
                 lastRefreshedUser,
                 isEnabled,
                 iocStoreConfig,
-                iocTypes
+                iocTypes,
+                true
         );
     }
 }
