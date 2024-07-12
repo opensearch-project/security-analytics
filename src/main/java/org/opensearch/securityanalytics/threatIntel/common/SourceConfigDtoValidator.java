f7b90e3be0b17d2feca8c5a733718e4327580f09
AWSHurneyt <hurneyt@amazon.com>
2024-07-12 10:32:41 -0700

    Adjusted IOCTypes usage. (#1156)
    * Removed TODOs.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Refactored how STIX2IOCGenerator creates IOCs of specific types.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Added additional integration tests.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Changed IOCType usage as it's no longer an enum in SA commons.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Updated jar file.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Fixed unit tests.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Fixed tests.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Refactored build.gradle to exclude redundant dependencies from SA commons, instead of the SA commons jar being generated withhout those dependencies.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Updated jar.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Reverted changes to build.gradle.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Updated jar.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Fixed tests.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Fixed tests.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Fixed tests.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Fixed IOCType usage.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Fixed log message.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Fixed tests.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Addressed PR feedback.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    ---------
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    (cherry picked from commit a6eb64d30d5a6914b201b81491a406a297f13af8)
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>

--- src/main/java/org/opensearch/securityanalytics/threatIntel/common/SourceConfigDtoValidator.java
+++ src/main/java/org/opensearch/securityanalytics/threatIntel/common/SourceConfigDtoValidator.java
@@ -5,6 +5,7 @@
 
 package org.opensearch.securityanalytics.threatIntel.common;
 
+import org.opensearch.securityanalytics.commons.model.IOC;
 import org.opensearch.securityanalytics.commons.model.IOCType;
 import org.opensearch.securityanalytics.threatIntel.model.IocUploadSource;
 import org.opensearch.securityanalytics.threatIntel.model.S3Source;
@@ -22,15 +23,12 @@ import java.util.stream.Collectors;
 public class SourceConfigDtoValidator {
     public List<String> validateSourceConfigDto(SATIFSourceConfigDto sourceConfigDto) {
         List<String> errorMsgs = new ArrayList<>();
-        List<String> iocTypeEnumNames = Arrays.stream(IOCType.values())
-                .map(Enum::name)
-                .collect(Collectors.toList());
 
         if (sourceConfigDto.getIocTypes().isEmpty()) {
             errorMsgs.add("Must specify at least one IOC type");
         } else {
             for (String s: sourceConfigDto.getIocTypes()) {
-                if (false == iocTypeEnumNames.contains(s)) {
+                if (!IOCType.supportedType(s)) {
                     errorMsgs.add("Invalid IOC type: " + s);
                 }
             }
