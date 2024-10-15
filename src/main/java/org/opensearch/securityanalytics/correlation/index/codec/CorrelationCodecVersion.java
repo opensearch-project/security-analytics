2210bd28f87e9d94a7ad076d54cece910810f9fa
AWSHurneyt <hurneyt@amazon.com>
2024-10-15 16:47:24 -0700

    Fixed Lucene99Codec import.
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>

--- src/main/java/org/opensearch/securityanalytics/correlation/index/codec/CorrelationCodecVersion.java
+++ src/main/java/org/opensearch/securityanalytics/correlation/index/codec/CorrelationCodecVersion.java
@@ -5,8 +5,8 @@
 package org.opensearch.securityanalytics.correlation.index.codec;
 
 import org.apache.lucene.codecs.Codec;
-import org.apache.lucene.codecs.lucene99.Lucene99Codec;
 import org.apache.lucene.backward_codecs.lucene95.Lucene95Codec;
+import org.apache.lucene.backward_codecs.lucene99.Lucene99Codec;
 import org.apache.lucene.codecs.perfield.PerFieldKnnVectorsFormat;
 import org.opensearch.index.mapper.MapperService;
 import org.opensearch.securityanalytics.correlation.index.codec.correlation950.CorrelationCodec950;
