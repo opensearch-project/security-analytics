58f2191ff6b0557006762742f7f48800453536dc
Subhobrata Dey <sbcd90@gmail.com>
2025-02-06 00:05:52 +0000

    fix version dependency issues
    Signed-off-by: Subhobrata Dey <sbcd90@gmail.com>

--- src/test/java/org/opensearch/securityanalytics/resthandler/ThreatIntelMonitorRestApiIT.java
+++ src/test/java/org/opensearch/securityanalytics/resthandler/ThreatIntelMonitorRestApiIT.java
@@ -1,8 +1,8 @@
 package org.opensearch.securityanalytics.resthandler;
 
 import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HttpStatus;
 import org.apache.hc.core5.http.io.entity.StringEntity;
-import org.apache.http.HttpStatus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
