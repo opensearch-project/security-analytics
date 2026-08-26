1db3dea5a4e6ec3ff19cc2a3ac3964eaf4b830b2
shreyah963 <shreyab963@gmail.com>
2026-08-26 15:40:33 -0700

    Add SPDX license header checker and missing headers
    Signed-off-by: shreyah963 <shreyab963@gmail.com>

--- src/main/java/org/opensearch/securityanalytics/threatIntel/sacommons/monitor/IndexIocScanMonitorResponseInterface.java
+++ src/main/java/org/opensearch/securityanalytics/threatIntel/sacommons/monitor/IndexIocScanMonitorResponseInterface.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.opensearch.securityanalytics.threatIntel.sacommons.monitor;
 
 public interface IndexIocScanMonitorResponseInterface {
